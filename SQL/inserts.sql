7120e0444a15c4e94b15519f9119abdd4ed9d480
JamesPCoyle <up741525@myport.ac.uk>
2018-02-21 15:46:14 +0000

    changed attribute names and updated inserts

--- SQL/inserts.sql
+++ SQL/inserts.sql
@@ -1,10 +1,10 @@
 insert into user(fname, lname, email, token) 
   values ('James', 'Coyle', 'jamescoyle@mail.com', '123456');
 
-insert into user(fname, lname, email, token, budget) 
+insert into user(fname, lname, email, token) 
   values ('John', 'Smith', 'johnsmith@mail.com', '654321');
 
-insert into balance(user, bdate, amount) 
+insert into balance(user, bdate, budget) 
   values (1, '2018/01/01', 60)
 
 insert into category(cname) 
@@ -13,8 +13,8 @@ insert into category(cname)
 insert into category(cname) 
   values ('transport');
 
-insert into transaction(user, amount, description, tdate, category, image) 
+insert into transaction(user, amount, description, tdate, category) 
   values (1, 20, 'shopping', '2018/01/01', 1);
 
 insert into transaction(user, amount, description, tdate, category, image) 
-  values (1, 15, 'travel', '2017/11/12', 2);
+  values (1, 15, 'travel', '2017/11/12', 2, 'file location');
