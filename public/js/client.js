a6cbbff2754db6ff97dc4deef0054c1eaa4521d0
Stefan Puia <stephano2013@yahoo.com>
2018-03-05 15:24:13 +0000

    made the login check synchronous

--- public/js/client.js
+++ public/js/client.js
@@ -28,9 +28,12 @@ window.addEventListener('load', async function() {
     $('#nav-log-mobile').on('click', signOut);
 
     // send the user to /login if not logged in
-    if (location.pathname != "/login" && await !gapi.auth2.getAuthInstance().isSignedIn.get()) {
-       signOut();
-    }
+    gapi.auth2.getAuthInstance().then(function(e) {
+        let logged = e.isSignedIn.get();
+        if (location.pathname != "/login" && logged === false) {
+            signOut();
+        }
+    })
     fillDash();
 })
 
