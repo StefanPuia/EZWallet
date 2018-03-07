8fef5df663410593a4c5f3fa4249b0c5a5d2fe54
ORogers <32385758+ORogers@users.noreply.github.com>
2018-03-07 02:32:45 +0000

    updated transaction query

--- app/core/api.js
+++ app/core/api.js
@@ -147,16 +147,15 @@ module.exports = function(app) {
     app.get('/api/transaction', function(req, res) {
         util.getUserId(req.user, function(err, user) {
             if (user) {
-                let columns = ['id', 'amount', 'description', 'tdate', 'category', 'image'];
-                let sql;
+                let sql = "select transaction.amount as Amount, transaction.description as Description, transaction.tdate as Date, category.cname as Category, transaction.image as Image from transaction inner join category on transaction.category = category.id";
                 let inserts;
                 //adds date filter if perameters are present
                 if (req.query.month == undefined || req.query.year == undefined) {
-                    sql = 'SELECT ?? FROM ?? WHERE ?? = ?';
-                    inserts = [columns, 'transaction', 'user', user.id];
+                    sql += 'WHERE transaction.user = ?';
+                    inserts = [user.id];
                 } else {
-                    sql = 'SELECT ?? FROM ?? WHERE ?? = ? AND MONTH(tdate) = ? AND YEAR(tdate) = ?';
-                    inserts = [columns, 'transaction', 'user', user.id, req.query.month, req.query.year];
+                    sql += ' WHERE transaction.user = ? AND MONTH(transaction.tdate) = ? AND YEAR(transaction.tdate) = ?';
+                    inserts = [user.id, req.query.month, req.query.year];
                 }
 
                 util.query(sql, inserts, function(results) {
